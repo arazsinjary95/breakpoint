e896c76c626267d09ecc8e7f5e4605c1dd608e65
Araz Sinjary <araz@Arazs-MBP.Home>
2018-06-21 08:20:05 +0300

    creating insettezxtfield and shadowview subclasses

---
+++ breakpoint/View/InsetTextField.swift
@@ -0,0 +1,39 @@
+//
+//  InsetTextField.swift
+//  breakpoint
+//
+//  Created by Araz Sinjary on 6/21/18.
+//  Copyright © 2018 Araz Sinjary. All rights reserved.
+//
+
+import UIKit
+
+class InsetTextField: UITextField {
+
+    private var padding = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
+    
+    //setup placeholder text so that's white.
+    override func awakeFromNib() {
+        let placeholder = NSAttributedString(string: self.placeholder!, attributes: [NSAttributedStringKey.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+        self.attributedPlaceholder = placeholder
+        super.awakeFromNib()
+    }
+    
+    //this for when we are just looking at text in the text rectangle without editing it or modifying it.
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    //this for when we actually typing text.
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+}
+
+
+
+
+
