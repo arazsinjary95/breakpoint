3e113b929d05ec3aec7575b1fdce1605655b0a56
Araz Sinjary <araz@Arazs-MBP.Home>
2018-06-21 12:54:05 +0300

    presenting loginvc from appdelegate / allowing login with email

--- breakpoint/AppDelegate.swift
+++ breakpoint/AppDelegate.swift
@@ -18,6 +18,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        //just check to see if the current user is nil.
+        if Auth.auth().currentUser == nil {
+            //if there is no user logged in we're going to ahead and present our log in.
+            let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+            let authVC = storyboard.instantiateViewController(withIdentifier: "AuthVC")
+            window?.makeKeyAndVisible()
+            window?.rootViewController?.present(authVC, animated: true, completion: nil)
+        }
         return true
     }
 
