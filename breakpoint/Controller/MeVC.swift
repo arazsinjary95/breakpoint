cd1e9b50736c948115227e1563095e3f657a5a5e
Araz Sinjary <araz@Arazs-MBP.Home>
2018-06-21 13:27:13 +0300

    building mevc and adding to uitabbarcontroller

---
+++ breakpoint/Controller/MeVC.swift
@@ -0,0 +1,28 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by Araz Sinjary on 6/21/18.
+//  Copyright © 2018 Araz Sinjary. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+        //sign out code
+    }
+    
+
+}
