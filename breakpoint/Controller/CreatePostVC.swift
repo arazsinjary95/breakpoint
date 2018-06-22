243b3c13ad40532c997a1b42adafd096ab90a623
Araz Sinjary <araz@Arazs-MBP.Home>
2018-06-22 12:13:12 +0300

    converting uids into emails and reversing the order of tableview

--- breakpoint/Controller/CreatePostVC.swift
+++ breakpoint/Controller/CreatePostVC.swift
@@ -23,6 +23,11 @@ class CreatePostVC: UIViewController {
         sendBtn.bindToKeyboard()
        // profileImage.bindToKeyboard()
     }
+    //this for email to show in current page.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.emailLbl.text = Auth.auth().currentUser?.email
+    }
 
     @IBAction func sendBtnWasPressed(_ sender: Any) {
         if textView.text != nil && textView.text != "Say something here..." {
