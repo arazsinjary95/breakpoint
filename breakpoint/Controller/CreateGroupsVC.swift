fe14273e2c815ec3f3aad8822f4a694c8f76dff7
Araz Sinjary <araz@Arazs-MacBook-Pro.local>
2018-06-22 17:28:51 +0300

    creating usercell

--- breakpoint/Controller/CreateGroupsVC.swift
+++ breakpoint/Controller/CreateGroupsVC.swift
@@ -20,6 +20,8 @@ class CreateGroupsVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     @IBAction func doneBtnWasPressed(_ sender: Any) {
@@ -28,3 +30,29 @@ class CreateGroupsVC: UIViewController {
     @IBAction func closeBtnWasPressed(_ sender: Any) {
     }
 }
+
+extension CreateGroupsVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell") as?
+            UserCell else { return UITableViewCell() }
+        let profileImage = UIImage(named: "defaultProfileImage")
+        
+        cell.configureCell(profileImage: profileImage!, email: "araz@sinjary.com", isSelected: true)
+        return cell
+    }
+    
+}
+
+
+
+
+
+
