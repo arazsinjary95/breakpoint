4504035a43c64c0ab23534d2cb6c39b35ddbc18f
Araz Sinjary <araz@Arazs-MacBook-Pro.local>
2018-06-23 21:49:46 +0300

    initializing group data for a group and presenting on groupfeedvc

--- breakpoint/Services/DataService.swift
+++ breakpoint/Services/DataService.swift
@@ -110,6 +110,21 @@ class DataService {
         }
     }
     
+    //this for groupfeedvc
+    func getEmailsFor(group: Group, handler: @escaping (_ emailArray: [String]) -> ()) {
+        var emailArray = [String]()
+        REF_USERS.observeSingleEvent(of: .value) { (userSnapshot) in
+            guard let userSnapshot = userSnapshot.children.allObjects as? [DataSnapshot] else { return }
+            for user in userSnapshot {
+                if group.members.contains(user.key) {
+                    let email = user.childSnapshot(forPath: "email").value as! String
+                    emailArray.append(email)
+                }
+            }
+            handler(emailArray)
+        }
+    }
+    
     func createGroup(withTitle title: String, andDescription description: String, forUserIds ids: [String], handler: @escaping (_ groupCreated: Bool) -> ()) {
         REF_GROUPS.childByAutoId().updateChildValues(["tit": title, "des":description, "mem": ids])
         handler(true)
